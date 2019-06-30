804e08b40ba24bd89b5ad657a182327bbe9e02d7
Abderrahmane Smimite <smimite@gmail.com>
2019-06-30 23:58:22 +0200

    app switching to swagger

--- app.py
+++ app.py
@@ -1,88 +1,33 @@
-from flask import Flask,jsonify,request,abort
-import json
-
-import base64
-import os
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-from cryptography.fernet import Fernet
-
-def genKey(password_provided):
-    password = password_provided.encode() 
-    salt = b'rrdX9A66qXtQZwzf' # You NEED TO CHANGE THIS (eg. os.urandom(16)) must be of type bytes
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=100000,
-        backend=default_backend()
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
-    return key
+from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
-'''
-Secure Transport (TLS) is mandatory.
-This snippet is minimalist on purpose so to keep processing on the memory
-'''
-@app.route("/", methods=('GET',))
-def hello():
-    return "App is Up!"
 
+SWAGGER_URL = '/api/docs' # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'http://petstore.swagger.io/v2/swagger.json' # Our API url (can of course be a local resource)
 
-@app.route("/encrypt", methods=['POST'])
-def encrypt():
-    '''
-    {
-        "key":"secretKey",
-        "plaintext":"Text containing sensitive data to be encrypted"
-    }
-    '''
-    req_data = request.get_json()
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={ # Swagger UI config overrides
+    'app_name': "Test application"
+    },
+    # oauth_config={ # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    # 'clientId': "your-client-id",
+    # 'clientSecret': "your-client-secret-if-required",
+    # 'realm': "your-realms",
+    # 'appName': "your-app-name",
+    # 'scopeSeparator': " ",
+    # 'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
 
-    try:
-        key = req_data['key']
-        plaintext = req_data['plaintext']
-    except:
-        abort(400)
+# Register blueprint at URL
+# (URL must match the one given to factory function above)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
-    message = plaintext.encode()
+app.run()
 
-    try:
-        f = Fernet(genKey(key))
-        encrypted = f.encrypt(message)
-    except:
-        abort(403)
-    
-    return encrypted
-
-@app.route("/decrypt", methods=['POST'])
-def decrypt():
-    '''
-    {
-        "key":"secretKey",
-        "ciphertext":"gAAAAABdGQQgxejDKPqkR9tMGdHsL0ewJr3z3TOZeNC7-0AxBIxCv3gjAmng4ZrIY668ovifRMl1_F_5O64Wjbhn0qsm2Vn7UjbEhOEvXFlFIaK1ichVONWHr0sMGD5s30TNf7_9LEKN"
-    }
-    '''
-    req_data = request.get_json()
-    try:
-        key = req_data['key']
-        ciphertext = req_data['ciphertext']
-    except:
-        abort(400)
-
-    encrypted = ciphertext.encode()
-    
-    try:
-        f = Fernet(genKey(key))
-        decrypted = f.decrypt(encrypted)
-    except:
-        abort(403)
-
-    return decrypted
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+# Now point your browser to localhost:5000/api/docs/
