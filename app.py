d6571f0a001cdee486c018a031c5f8867d54bf78
Abderrahmane Smimite <smimite@gmail.com>
2019-07-02 22:02:18 +0200

    WSGI production mode

--- app.py
+++ app.py
@@ -1,33 +1,112 @@
-from flask import Flask
-from flask_swagger_ui import get_swaggerui_blueprint
+from flask import Flask,jsonify,request,abort
+from flask_restplus import Resource, Api, fields, reqparse
+import json
 
-app = Flask(__name__)
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+from cryptography.fernet import Fernet
 
 
-SWAGGER_URL = '/api/docs' # URL for exposing Swagger UI (without trailing '/')
-API_URL = 'http://petstore.swagger.io/v2/swagger.json' # Our API url (can of course be a local resource)
+flask_app = Flask(__name__)
+app = Api(app = flask_app, 
+		  version = "1.0", 
+		  title = "EOF (Encryption On the Fly)", 
+		  description = "Manage real-time encryption for your app with a simple API. Based on Fernet implementation which uses 128-bit AES in CBC mode and PKCS7 padding, with HMAC using SHA256. Proof Of Concept API with NO INFORMATION STORED.")
 
-# Call factory function to create our blueprint
-swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL, # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
-    API_URL,
-    config={ # Swagger UI config overrides
-    'app_name': "Test application"
-    },
-    # oauth_config={ # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
-    # 'clientId': "your-client-id",
-    # 'clientSecret': "your-client-secret-if-required",
-    # 'realm': "your-realms",
-    # 'appName': "your-app-name",
-    # 'scopeSeparator': " ",
-    # 'additionalQueryStringParams': {'test': "hello"}
-    # }
-)
+name_space = app.namespace('v1', description='Encryption on the fly API')
 
-# Register blueprint at URL
-# (URL must match the one given to factory function above)
-app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
-app.run()
+def genKey(password_provided):
+    password = password_provided.encode() 
+    salt = b'rrdX9A66qXtQZwzf' # You NEED TO CHANGE THIS (eg. os.urandom(16)) must be of type bytes
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
+    return key
 
-# Now point your browser to localhost:5000/api/docs/
+'''
+Secure Transport (TLS) is mandatory.
+This snippet is minimalist on purpose so to keep processing on the memory
+'''
+EncryptThis = app.model('Encrypt This', {
+    'key': fields.String(required=True, description='Your encryption password that will be used to derivate the key'),
+    'plaintext': fields.String(required=True, description='Data to encrypt')
+})
+
+DecryptThis = app.model('Decrypt This', {
+    'key': fields.String(required=True, description='Your encryption password used to derivate the key'),
+    'ciphertext': fields.String(required=True, description='Data to decrypt')
+})
+
+@name_space.route('/health')
+class Health(Resource):
+    def get(self):
+        '''Return system health status'''
+        return {'status': 'Everything looks fine'}
+
+                                       
+@name_space.route('/encrypt')
+class Encryption(Resource):
+    '''
+    {
+        "key":"secretKey",
+        "plaintext":"Text containing sensitive data to be encrypted"
+    }
+    '''
+    @name_space.expect(EncryptThis)
+    def post(self):
+        '''Get a plaintext and the associated key and return ciphertext'''
+        try:
+            key = name_space.payload['key']
+            plaintext = name_space.payload['plaintext']
+        except:
+            abort(400)
+        
+        message = plaintext.encode()
+        
+        try:
+            f = Fernet(genKey(key))
+            encrypted = f.encrypt(message)
+        except:
+            abort(403)
+        
+        return {'ciphertext':encrypted.decode()}
+
+@name_space.route('/decrypt')
+class Decryption(Resource):
+    '''
+    {
+        "key":"secretKey",
+        "ciphertext":"gAAAAABdGQQgxejDKPqkR9tMGdHsL0ewJr3z3TOZeNC7-0AxBIxCv3gjAmng4ZrIY668ovifRMl1_F_5O64Wjbhn0qsm2Vn7UjbEhOEvXFlFIaK1ichVONWHr0sMGD5s30TNf7_9LEKN"
+    }
+    '''
+    @name_space.expect(DecryptThis)
+    def post(self):
+        '''Get a ciphertext and the associated key and return plaintext'''
+        try:
+            key = name_space.payload['key']
+            ciphertext = name_space.payload['ciphertext']
+        except:
+            abort(400)
+
+        encrypted = ciphertext.encode()
+        
+        try:
+            f = Fernet(genKey(key))
+            decrypted = f.decrypt(encrypted)
+        except:
+            abort(403)
+
+        return {'plaintext':decrypted.decode()}
+
+if __name__ == '__main__':
+    flask_app.run(debug=False)
